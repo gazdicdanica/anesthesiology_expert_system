9a62a3f4a135443808ac1e48418a966b5154b192
Danica Gazdić <danicagazdic@gmail.com>
2024-06-10 15:20:06 +0200

    Fixed breath event sim

--- python/postoperative_data.py
+++ python/postoperative_data.py
@@ -25,9 +25,10 @@ def send_request(sap, pulseOximetry, breath, heartBeat):
 while True:
     sap = random.randint(50, 145)
     pulseOximetry = random.randint(90, 100)
-    breath = random.choices([False, True], weights=[3, 1], k=1)[0]
+    breath = random.choices([True, False], weights=[3, 1], k=1)[0]
     heartBeat = True
 
     send_request(sap, pulseOximetry, breath, heartBeat)
     time.sleep(1.4)
+    send_request(sap, pulseOximetry, breath, heartBeat)
     counter += 1
